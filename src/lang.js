8086838f88e1462a41cdae06fa3148154356796f
Dmitriy Alyoshin <valerich1715@gmail.com>
2023-01-12 18:14:46 +0100

    Created branch

---
+++ src/lang.js
@@ -0,0 +1,34 @@
+const langArr = {
+    "site-nav-1" :  {        
+        "en": "Home",
+        "ua": "Головна",
+    }, 
+    "site-nav-2" :  {        
+        "en": "How it’s made?",
+        "ua": "Як це зроблено?",
+    },
+    "site-nav-3" :  {        
+        "en": "Our products",
+        "ua": "Наші продукти",
+    },
+    "site-nav-4" :  {        
+        "en": "Contact",
+        "ua": "Контакти",
+    },
+       "btn-header": {        
+        "en": "Buy now &#707",
+        "ua": "Купити зараз &#707",
+    }, 
+     "hero" :  {        
+        "en": "Ice cream made with passion",
+        "ua": "Морозиво, зроблене з пристрастю",
+    }, 
+    "hero-franchise-1" :  {        
+        "en": "cafes",
+        "ua": "кафе",
+    },
+     "hero-franchise-2" :  {        
+        "en": "food trucks",
+        "ua": "фудтраків",
+    },     
+}
