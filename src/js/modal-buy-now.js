c363406aeb5f3a35b91fcb4fd5a34ddbb47c5e10
Dmitriy Alyoshin <valerich1715@gmail.com>
2023-01-14 20:15:11 +0100

    add modal

---
+++ src/js/modal-buy-now.js
@@ -0,0 +1,33 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    buttonOff: document.querySelector("[data-modal-open]"),
+    buttonOn: document.querySelector("[data-modal-close]"),
+    
+    body: document.body,
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+  
+
+  refs.buttonOff.addEventListener('click', disableScroll);
+  refs.buttonOn.addEventListener('click', enableScroll);
+
+
+  function disableScroll() {
+    refs.body.classList.add("disable-scroll");
+  }
+  
+
+  function enableScroll() {
+    refs.body.classList.remove("disable-scroll");
+  }
+})();
+
