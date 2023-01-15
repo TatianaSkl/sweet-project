c5121632866e12411f0eb9f5ac5ccd393e2cad02
Dmitriy Alyoshin <valerich1715@gmail.com>
2023-01-15 16:20:47 +0100

    Update mobile menu

--- src/js/modal-mobile-menu.js
+++ src/js/modal-mobile-menu.js
@@ -2,16 +2,20 @@
   const refs = {
     openModalBtn: document.querySelector("[data-mobile-menu-open]"),
     closeModalBtn: document.querySelector("[data-mobile-menu-close]"),
+    closeModalBtn1: document.querySelector("[data-mobile-menu-close-1]"),
+
     modal: document.querySelector("[data-mobile-menu]"),
     
     buttonOff: document.querySelector("[data-mobile-menu-open]"),
     buttonOn: document.querySelector("[data-mobile-menu-close]"),
+    buttonOn1: document.querySelector("[data-mobile-menu-close-1]"),
     
     body: document.body,
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn1.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
@@ -20,7 +24,7 @@
 
   refs.buttonOff.addEventListener('click', disableScroll);
   refs.buttonOn.addEventListener('click', enableScroll);
-
+  refs.buttonOn1.addEventListener('click', enableScroll);
 
   function disableScroll() {
     refs.body.classList.add("disable-scroll");
