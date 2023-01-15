47aea73bbe90746358515de21694aaed16134cef
Dmitriy Alyoshin <valerich1715@gmail.com>
2023-01-15 16:50:14 +0100

    Update js

--- src/js/modal-mobile-menu.js
+++ src/js/modal-mobile-menu.js
@@ -16,21 +16,19 @@
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn1.addEventListener("click", toggleModal);
-
+  
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
   
-
   refs.buttonOff.addEventListener('click', disableScroll);
   refs.buttonOn.addEventListener('click', enableScroll);
   refs.buttonOn1.addEventListener('click', enableScroll);
-
+  
   function disableScroll() {
     refs.body.classList.add("disable-scroll");
   }
   
-
   function enableScroll() {
     refs.body.classList.remove("disable-scroll");
   }
