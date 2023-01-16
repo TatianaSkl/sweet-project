94007dfa95159bb89a673cc65cdfd39391f3f78f
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2023-01-16 16:04:36 +0000

    Deploying to gh-pages from @ TatianaSkl/sweet-project@27602894ad48eaf49ace674959cf91c8e08bc689 🚀

---
+++ index.e53e8e47.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".menu a").click((function(){elementClick=$(this).attr("href"),destination=$(elementClick).offset().top,$("body,html").animate({scrollTop:destination},1400)}))}));
+//# sourceMappingURL=index.e53e8e47.js.map
