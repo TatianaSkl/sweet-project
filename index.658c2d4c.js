cb7ae7138210092fd950765f3f88511aa1352fee
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2023-01-16 14:39:12 +0000

    Deploying to gh-pages from @ TatianaSkl/sweet-project@606f28944060419dde62309efb2c4ba67eb20bb6 🚀

---
+++ index.658c2d4c.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({dots:!1,infinite:!0,speed:400,slidesToShow:1,adaptiveHeight:!0,arrows:!1,autoplay:!0}),$(".reviews-slick").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0,arrows:!1});
+//# sourceMappingURL=index.658c2d4c.js.map
